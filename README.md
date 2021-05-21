9871b8919ba87bdf908ef440066b97ad8d2ad270
shivamsahu111 <ssahu5407@gmail.com>
2021-05-21 18:19:16 +0530

    My HMS website

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# SMIThms.github.io
+# Online-Hostel-management-system-Project
+
+This Projects Demo Video link : https://youtu.be/VlN6EuCBmeY
+
+Another Project link : https://youtu.be/xnnS5XQe7p4
---
+++ footer.php
@@ -0,0 +1,9 @@
+
+<div class="foot">
+
+<p> <a href="https://mail.google.com/mail/u/1/#inbox"target="_blank">ssahu5407@gmail.com </br>
+<a href="https://www.facebook.com/Shivamsahu111.1"target="_blank">SHIVAM SAHU</p>
+
+<style> .foot{text-align:center; border: 2px solid black;}</style>
+
+</div>
